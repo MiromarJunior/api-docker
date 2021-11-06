46e2aa259515479cc235d99c3a384aa27a47ce76
MIromar Junior <miromarjunior@gmail.com>
2021-11-06 17:16:08 -0300

    save edit e lis pronto

--- src/main/java/br/mso/apispringdocker/ApiDockerApplication.java
+++ src/main/java/br/mso/apispringdocker/ApiDockerApplication.java
@@ -2,8 +2,10 @@ package br.mso.apispringdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication
+@EntityScan(basePackages = "br.mso.apispringdocker.model")
+@SpringBootApplication(scanBasePackages = "br.mso.apispringdocker")
 public class ApiDockerApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/br/mso/apispringdocker/controller/dto/PessoaRq.java
@@ -0,0 +1,25 @@
+package br.mso.apispringdocker.controller.dto;
+
+public class PessoaRq {
+	
+	private String nome;
+	private String Sobrenome;
+	
+	
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getSobrenome() {
+		return Sobrenome;
+	}
+	public void setSobrenome(String sobrenome) {
+		Sobrenome = sobrenome;
+	}
+	
+	
+
+}
